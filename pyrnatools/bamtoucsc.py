0f1c286a17cd18ee5ed289eaf4a8bc148a90820c
Patrick LOmbard <pdl30@cam.ac.uk>
2014-11-18 15:38:21 +0000

    rewrite bw names

--- pyrnatools/bamtoucsc.py
+++ pyrnatools/bamtoucsc.py
@@ -16,7 +16,7 @@ import pysam
 import pybedtools
 import pkg_resources
 
-def convert_bam_bed(name):
+def convert_bam_bed(name, paired):
 	count = 0
 	print "==> Converting bam to bed...\n"
 #	if aligner=="T":
@@ -38,10 +38,12 @@ def convert_bam_bed(name):
 #		inbam = pybedtools.BedTool(name+".bam")
 #		bed = inbam.bam_to_bed(split=True)
 #		bed.saveas(name+".BED")
+	if paired:
+		count /= 2
 	return count
 
 def change_ens_ucsc_for_bed(name):
-	outbed2 = open(name+".ucsc.BED", "w")
+	outbed2 = open(name+"_ucsc.BED", "w")
 	print "==> Converting Ensembl to UCSC chromosomes...\n"
 	with open(name+".BED") as f:
 		for line in f:
@@ -60,18 +62,25 @@ def change_ens_ucsc_for_bed(name):
 			outbed2.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(new_chr, word[1], word[2], word[3], word[4], word[5])),
 
 ##Must include scaling!
-def genomeCoverage(name, scale=None):
+def genomeCoverage(name, house, rpm):
 	print "==> Converting bed to bedGraph...\n"
 	inbed = pybedtools.BedTool(name+".BED")
-	if scale:
-		outcov = inbed.genome_coverage(bg=True, genome='mm10', scale=scale)
+	if house:
+		outcov = inbed.genome_coverage(bg=True, genome='mm10', scale=house)
+		output = name+"_house.bedGraph"
+	elif rpm:
+		outcov = inbed.genome_coverage(bg=True, genome='mm10', scale=rpm)
+		output = name+"_rpm.bedGraph"
 	else:
 		outcov = inbed.genome_coverage(bg=True, genome='mm10')
-	outcov.saveas(name+".bedGraph")
+		output = name+".bedGraph"
+	outcov.saveas(output)
+	return output
 
-def bedgraphtobigwig(name, chrom):
+def bedgraphtobigwig(bedgraph, chrom, house, rpm):
+	bw = re.sub(".bedGraph$", ".bw", bedgraph)
 	print "==> Converting bedGraph to bigWig...\n"
-	command = ["bedGraphToBigWig", name+".bedGraph", chrom, name+".bw"]
+	command = ["bedGraphToBigWig", bedgraph, chrom, bw]
 	subprocess.call(command)
 
 def normalise_to_housekeeper(count_file):
@@ -88,7 +97,7 @@ def normalise_to_housekeeper(count_file):
 def main():
 	parser = argparse.ArgumentParser(description="Processes RNA-seq samples to bigWig tracks.\nIf Tophat2 is specified, this will pull out the uniquely mapped reads\nOtherwiseit is assumed that the bam file is already uniquely mapped!")
 	parser.add_argument('-i', '--input', help='Bam file from aligner etc.', required=True)
-#	parser.add_argument('-a','--ALIGNER', help='Aligner used. Valid choices are T/S for tophat2 and star respectively', required=True)
+	parser.add_argument('-p', action='store_true', help='Use if samples are paired end.', required=False)
 	parser.add_argument('-g', '--genome', help='Genome the samples are aligned to, options include mm10/mm9/hg19', required=True)
 	parser.add_argument('-rpm', action='store_true', help='Scale to RPM', required=False) 
 	parser.add_argument('-a', '--house', help='Housekeeper normalisation. Input file is HTSEQ-count file containing gene for normalisation on first line', required=False)
@@ -101,17 +110,20 @@ def main():
 	
 	name = re.sub(".bam$", "", args["input"])
 
-	unique_reads = convert_bam_bed(name)
+	unique_reads = convert_bam_bed(name, args["p"])
+	#print unique_reads
+	if args["ens"]:
+		change_ens_ucsc_for_bed(name)
+		name = name+"_ucsc"
+
 	if args["rpm"]:
 		scale = float(1000000)/int(unique_reads)
+		bedgraph = genomeCoverage(name, rpm=scale)	
 	elif args["house"]:
 		house = normalise_to_housekeeper(args["house"])
 		scale = float(1000)/int(house) #Works and checked
+		bedgraph = genomeCoverage(name, house=scale)
 	else:
-		scale = 1
+		bedgraph = genomeCoverage(name)
 	
-	if args["ens"]:
-		change_ens_ucsc_for_bed(name)
-		name = name+".ucsc"
-	genomeCoverage(name, scale)		
-	bedgraphtobigwig(name, chrom)
+	bedgraphtobigwig(bedgraph, chrom)
