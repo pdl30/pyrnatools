03e58d9796541aa4d8e2554b6ce33d35e290b51f
Patrick LOmbard <pdl30@cam.ac.uk>
2015-03-27 11:41:33 +0000

    fixing dexseq

--- pyrnatools/splicing.py
+++ pyrnatools/splicing.py
@@ -48,19 +48,20 @@ def dexseq_prep(sample, dexseq_dir, gtf, paired, orientation):
 	count_program = os.path.join(dexseq_dir, "python_scripts/dexseq_count.py")
 	bam_name = os.path.basename(sample)
 	output = re.sub(".bam$", "_dexseq.count", bam_name)
+	output = open("count_output.txt", "a")
 	if paired:
 		p = "yes"
 	else:
 		p = "no"
 	command = "python {} -f bam -p {} -s {} {} {} {}".format(count_program, p, orientation, gtf, sample, output)
 	#python dexseq_count.py -s reverse Homo_sapiens.GRCh37.74.ucsc_dexseq.gff  /accepted_hits.bam 75K.DIF.1_1.fq.mm10.count -f bam -p yes
-	subprocess.call(command.split())
+	subprocess.call(command.split(), stdout=output)
 
 def create_design_for_R(idict):
 	output = open("tmp_design.txt", "w")
 	output.write("sampleName\tfileName\tcondition\n"),
 	for key in sorted(idict.keys()):
-		bam_name = os.path.basename(sample)
+		bam_name = os.path.basename(key)
 		name = re.sub(".bam$", "", bam_name)
 		count = re.sub(".bam$", "_dexseq.count", bam_name)
 		output.write("{}\t{}\t{}\n".format(name, count, idict[key]))
@@ -69,52 +70,16 @@ def create_design_for_R(idict):
 def dexseq_run(conditions, comp1, comp2, gtf):
 	rscript = "suppressPackageStartupMessages(library(DEXSeq))\n"
 	rscript += "pdata <- read.table('tmp_design.txt', header=T)\n"
-	rscript += "counts1 <- pdata[which(pdata[,2] == '{}'),]\n".format(comp1)
-	rscript += "counts2 <- pdata[which(pdata[,2] == '{}'),]\n".format(comp2)
+	rscript += "counts1 <- pdata[which(pdata[,3] == '{}'),]\n".format(comp1)
+	rscript += "counts2 <- pdata[which(pdata[,3] == '{}'),]\n".format(comp2)
 	rscript += "new_pdata <- rbind(counts1, counts2)\n"
-	rscript += "dxd <- DEXSeqDataSetFromHTSeq(as.character(new_pdata[,2]), sampleData=new_pdata, design= ~ sample + exon + condition:exon, flattenedfile={})\n".format(gtf)
+	rscript += "dxd <- DEXSeqDataSetFromHTSeq(as.character(new_pdata[,2]), sampleData=new_pdata, design= ~ sample + exon + condition:exon, flattenedfile='{}')\n".format(gtf)
 	rscript += "dxd = estimateSizeFactors( dxd )\n"
 	rscript += "dxd = estimateDispersions( dxd )\n"
 	rscript += "dxd = testForDEU( dxd )\n"
 	rscript += "dxr1 = DEXSeqResults( dxd )\n"
 	rscript += "DEXSeqHTML( dxr1, FDR=0.1, color=c('#FF000080', '#0000FF80') )\n"
 	rscript += "write.table(dxr1, file='{}_vs_{}.tsv', sep='\\t', quote=F)\n".format(comp1, comp2)
-	return rscript
-
-def seqgsea_run(conditions, comp1, comp2):
-	rscript = "suppressPackageStartupMessages(library('SeqGSEA'))\n"
-	rscript += "pdata <- read.table('tmp_design.txt', header=T)\n"
-	rscript += "counts1 <- pdata[which(pdata[,2] == '{}'),]\n".format(comp1)
-	rscript += "counts2 <- pdata[which(pdata[,2] == '{}'),]\n".format(comp2)
-	rscript += "RCS <- loadExonCountData(as.character(counts1), as.character(counts2))\n"
-	rscript += "RCS <- exonTestability(RCS, cutoff=5)\n"
-	rscript += "geneTestable <- geneTestability(RCS)\n"
-	rscript += "RCS <- subsetByGenes(RCS, unique(geneID(RCS))[ geneTestable ])\n"
-	rscript += "geneIDs <- unique(geneID(RCS))\n"
-	rscript += "RCS <- estiExonNBstat(RCS)\n"
-	rscript += "RCS <- estiGeneNBstat(RCS)\n"
-	rscript += "perm.times <- 1000\n"
-	rscript += "permuteMat <- genpermuteMat(RCS, times=perm.times)\n"
-	rscript += "RCS <- DSpermute4GSEA(RCS, permuteMat)\n"
-	rscript += "DSscore.normFac <- normFactor(RCS@permute_NBstat_gene)\n"
-	rscript += "DSscore <- scoreNormalization(RCS@featureData_gene$NBstat, DSscore.normFac)\n"
-	rscript += "DSscore.perm <- scoreNormalization(RCS@permute_NBstat_gene, DSscore.normFac)\n"
-	rscript += "RCS <- DSpermutePval(RCS, permuteMat)\n"
-	return rscript
-
-def spliceR(idir, gtf, genome):
-	#Uses cuffdiff directories. Not working currently, giving errors
-	rscript = "library(spliceR)\n"
-	rscript += "cuffDB <- readCufflinks(dir={},gtf={},genome='{}')\n"
-	rscript += "cuffDB_spliceR <- prepareCuff(cuffDB)\n"
-	rscript += "myTranscripts <- transcripts(cuffDB_spliceR); myExons <- exons(cuffDB_spliceR); conditions(cuffDB_spliceR)\n"
-	#rscript += "cuffDB_spliceR_filtered <- preSpliceRFilter(cuffDB_spliceR,filters=c('expressedIso', 'isoOK', 'expressedGenes', 'geneOK'))\n"
-	rscript += "mySpliceRList <- spliceR(cuffDB_spliceR, compareTo='preTranscript', filters=c('expressedGenes','geneOK', 'isoOK', 'expressedIso', 'isoClass'))\n"
-	rscript += "ucscCDS <- getCDS(selectedGenome='hg19', repoName='UCSC'); require('BSgenome.Hsapiens.UCSC.hg19', character.only = TRUE)\n"
-	rscript += "PTCSpliceRList <- annotatePTC(cuffDB_spliceR, cds=ucscCDS, Hsapiens, PTCDistance=50)\n"
-	rscript += "generateGTF(mySpliceRList, filters=c('geneOK', 'isoOK', 'expressedGenes', 'expressedIso'), scoreMethod='local', useProgressBar=F)\n"
-	rscript += "mySpliceRList <- spliceRPlot(mySpliceRList, evaluate='nr_transcript_pr_gene')\n"
-	rscript += "mySpliceRList <- spliceRPlot(mySpliceRList, evaluate='mean_AS_transcript', asType='ESI')\n"
 	return rscript
 
 def mats(conditions, comp1, comp2, gtf, insert, sd, rlen, mats_program):
@@ -159,7 +124,6 @@ def run_rcode(rscript, name):
 def dexseq_prep_fun(args):
 	return dexseq_prep(*args)
 
-
 def main():
 	parser = argparse.ArgumentParser(description='Overview of a few programs for Splicing analysis\n')
 	subparsers = parser.add_subparsers(help='Programs included',dest="subparser_name")
@@ -178,7 +142,7 @@ def main():
 	dexseq_parser.add_argument('-g','--gtf', help='GTF file formatted by dexseq using deseq_prepare_annotation.py', required=True)
 	dexseq_parser.add_argument('-t','--threads', help='threads, default=1', default=1, required=False)
 	dexseq_parser.add_argument('-p', action='store_true', help='Use if samples are paired end. Will find sd and insert size for bam files', required=False)
-	dexseq_parser.add_argument('-o','--orientation', help='Options are yes, no or reverse. Test First!!!', required=True)
+	dexseq_parser.add_argument('-o','--orientation', help='Options are yes, no or reverse. Test First!!!', required=False)
 
 	mats_parser = subparsers.add_parser('mats', help="Runs MATS")
 	mats_parser.add_argument('-c','--config', help='Config file containing bam files, please see documentation for usage!', required=True)
@@ -186,24 +150,11 @@ def main():
 	mats_parser.add_argument('-n','--insert', help='Insert size, use for paired end data. In order to make things simple, will assume all bams are the same', default=None, required=False)
 	mats_parser.add_argument('-s','--sd', help='SD', required=False)
 	mats_parser.add_argument('-r','--len', help='read length', required=False)
-	
-#	splice_parser = subparsers.add_parser('spliceR', help="Runs spliceR")
-#	splice_parser.add_argument('-c','--config', help='Config file containing bam files, please see documentation for usage!', required=True)
-#	splice_parser.add_argument('-g','--gtf', help='GTF file formatted by dexseq', required=True)
-#	splice_parser.add_argument('-o','--output', help='Output directory', required=True)
-
-#	seq_parser = subparsers.add_parser('seqGSEA', help="Runs seqGSEA")
-#	seq_parser.add_argument('-c','--config', help='Config file containing bam files, please see documentation for usage!', required=True)
-#	seq_parser.add_argument('-g','--gtf', help='GTF file formatted by prepare_exon_annotation_ensembl.py script', required=True)
-#	seq_parser.add_argument('-t','--threads', help='threads, default=1', default=1, required=False)
-#	seq_parser.add_argument('-p', action='store_true', help='Use if samples are paired end. Will find sd and insert size for bam files', required=False)
-#	seq_parser.add_argument('-o','--orientation', help='Options are yes, no or reverse. Test First!!!', required=True)
-
 	if len(sys.argv)==1:
 		parser.print_help()
 		sys.exit(1)
 	args = vars(parser.parse_args())
-	
+
 	Config = ConfigParser.ConfigParser()
 	Config.optionxform = str
 	Config.read(args["config"])
@@ -221,6 +172,7 @@ def main():
 			pool.close()
 			pool.join()
 		else:
+			create_design_for_R(conditions)
 			comparisons = ConfigSectionMap("Comparisons", Config)
 			for comp in comparisons:
 					c = comparisons[comp].split(",")
@@ -235,19 +187,3 @@ def main():
 			c = comparisons[comp].split(",")
 			comps = [x.strip(' ') for x in c]
 			mats(conditions, comps[0], comps[1], args["gtf"], args["insert"], args["sd"], args["len"], mats_program)
-
-#	elif args["subparser_name"] == "seqGSEA":
-#		path = "/raid/home/patrick/R/x86_64-pc-linux-gnu-library/3.1/SeqGSEA/extscripts"
-#		count_program = path + "/count_in_exons.py"
-#		pool = Pool(int(args["threads"]))
-#		pool.map(seqgsea_count_fun, itertools.izip(list(conditions.keys()), itertools.repeat(count_program), itertools.repeat(args["gtf"]), itertools.repeat(args["p"]),
-#			itertools.repeat(args["orientation"]))) ##Running annotation in parallel
-#		pool.close()
-#		pool.join()
-	#	comparisons = ConfigSectionMap("Comparisons", Config)
-	#	for comp in comparisons:
-	#		c = comparisons[comp].split(",")
-	#		comps = [x.strip(' ') for x in c]
-	##		rscipt = run_seqgsea(conditions, comps[0], comps[1])
-		#	run_rcode(rscript, "dexseq.R")
-main()
